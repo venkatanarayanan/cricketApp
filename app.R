70bc47e0863c54c911cb99326c46650049e23462
unknown <venkatanarayanan22@gmail.com>
2021-06-15 15:43:53 +0530

    Replaced duplicate code with reactive expression

--- app.R
+++ app.R
@@ -5,13 +5,14 @@ ui <- fluidPage(
   tableOutput("table")
 )
 server <- function(input, output, session){
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
   output$summary <- renderPrint({
-    dataset <- get(input$dataset, "package:datasets")
-    summary(dataset)
+    summary(dataset())
   })
   output$table <- renderTable({
-    dataset <- get(input$dataset, "package:datasets")
-    dataset
+    dataset()
   })
 }
 shinyApp(ui, server)
